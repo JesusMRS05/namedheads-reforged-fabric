c7c8475d34572b5f5941ea505763243d41e75457
JesusMRS05 <jususmrey@gmail.com>
2026-01-11 04:17:03 +0100

    chore: change modid, package name, client class name. add authors, description, mod name.

--- src/client/java/com/github/jesusmrs05/mixin/client/ExampleClientMixin.java
+++ src/client/java/com/github/jesusmrs05/mixin/client/ExampleClientMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin.client;
+package com.github.jesusmrs05.mixin.client;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
--- src/main/java/com/github/jesusmrs05/mixin/ExampleMixin.java
+++ src/main/java/com/github/jesusmrs05/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.jesusmrs05.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
